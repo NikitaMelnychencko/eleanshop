b68a3298bcdf1bb1d7c3e5c57b6d2584f796adc3
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-19 23:36:56 +0300

    add-js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,48 @@
-import name from './js/modual-1'
-import './images/leptop.jpg'
+import './images/img/exampl.jpg'
 import './images/svg/icon.svg'
 import './sass/main.scss'
-console.log(name);
-console.log('это index.js');
-console.log('это uuu.js');
-const refBox = document.querySelector('div')
-//refBox.insertAdjacentHTML('beforeend', `<img src="${example}" alt="leptop">`);
-
-
+//=========layout===============
+//static
+import './js/layout/static/header'
+import './js/layout/static/footer'
+//main
+import './js/layout/main/hero'
+import './js/layout/main/contend'
+import './js/layout/main/aboutTheBrand'
+import './js/layout/main/ourShowRoom'
+import './js/layout/main/registrationFormForFitting'
+import './js/layout/main/inInstgram'
+//catalog
+import './js/layout/сatalog/filter';
+import './js/layout/сatalog/gallery';
+//product
+import './js/layout/product/infoAboutProduct';
+import './js/layout/product/recomendationsCategory';
+import './js/layout/product/producCreatetInfo';
+//delivery
+import './js/layout/delivery/filter';
+import './js/layout/delivery/deliveryTypes';
+import './js/layout/delivery/formsQuestion';
+import './js/layout/delivery/questionsAndAnswers';
+//reviews
+import './js/layout/reviews/videoSet';
+import './js/layout/reviews/formsReviews';
+import './js/layout/reviews/registrationFormForFitting';
+//showroom
+import './js/layout/showroom/showroom';
+//contact
+import './js/layout/contact/contact';
+//fitting
+import './js/layout/fitting/informationAboutFitting';
+import './js/layout/fitting/sizeTable';
+import './js/layout/fitting/videoSlider';
+import './js/layout/fitting/formFittingInShowroom';
+//brand
+import './js/layout/brand/infoAboutBrand';
+import './js/layout/brand/ourAdvantages';
+import './js/layout/brand/formFittingInShowroom';
+//checkout
+import './js/layout/checkout/ordering';
+import './js/layout/checkout/payment';
+//favorites
+import './js/layout/favorites/favorites';
