3dd93932f804c49c8f263d31de9e3ccacdc7cc4f
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-09-26 12:37:09 +0300

    test brunch

---
+++ src/js/layout/fitting/sizeTable.js
@@ -0,0 +1,13 @@
+const main = document.querySelector('main');
+
+//import cardTp from './src/views/partials/fitting/sizeTable.hbs';
+
+import cardTp from '../../../views/partials/fitting/sizeTable.hbs';
+
+main.insertAdjacentHTML('afterbegin', cardTp());
+
+
+
+//  afterbegin  beforeend
+
+
