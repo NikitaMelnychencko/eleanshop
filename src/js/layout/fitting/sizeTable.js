be4679c5d4e1703ea5d70742e517ef3ce7d59892
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-09-26 22:49:54 +0300

    finish create markup

--- src/js/layout/fitting/sizeTable.js
+++ src/js/layout/fitting/sizeTable.js
@@ -1,13 +1,20 @@
-const main = document.querySelector('main');
-
-//import cardTp from './src/views/partials/fitting/sizeTable.hbs';
-
-import cardTp from '../../../views/partials/fitting/sizeTable.hbs';
-
-main.insertAdjacentHTML('afterbegin', cardTp());
+import size from '../../json/sizeTable.json';
+import createMarkup from '../../../views/partials/fitting/sizeTable.hbs';
 
 
 
-//  afterbegin  beforeend
+const body = document.querySelector('body');
+
+
+
+body.insertAdjacentHTML('afterbegin', createMarkup());
+
+
+//вешаю клас на боди что бы не скролилась страница
+body.classList.toggle('.sizeTable__no-scrol');
+
+
+console.log(size);
+
 
 
