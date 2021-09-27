33c6981138e2eae840cd3210be8bcbc4925d7d5b
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-09-27 08:46:54 +0300

    create markup

---
+++ src/js/layout/product/recomendationsCategory.js
@@ -0,0 +1,20 @@
+import '../../../images/svg/1.svg';
+import '../../../images/img/product/recomend-category/product-mobile-1.jpg';
+import '../../../images/img/product/recomend-category/product-mobile-2.jpg';
+import '../../../images/img/product/recomend-category/product-mobile-3.jpg';
+import '../../../images/img/product/recomend-category/product-mobile-1@2x.jpg';
+import '../../../images/img/product/recomend-category/product-mobile-2@2x.jpg';
+import '../../../images/img/product/recomend-category/product-mobile-3@2x.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-1.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-2.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-3.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-4.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-1@2x.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-2@2x.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-3@2x.jpg';
+import '../../../images/img/product/recomend-category/product-desktop-4@2x.jpg';
+
+import cards from '../../../views/partials/product/recomendationsCategory.hbs';
+console.log(cards);
+
+const data = cards();
