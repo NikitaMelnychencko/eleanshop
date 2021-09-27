10d77b1b16fb41c2ec14f9e5ccb1c494a354c003
Fosa1990 <fosa1990@gmail.com>
2021-09-27 21:33:21 +0300

    Update refs.js

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,6 +1,6 @@
 export default {
   bodyEl: document.querySelector('body'),
   mainEL: document.querySelector('main'),
-  pageHeroEL: document.querySelector('js-page-hero'),
-  pageHeroByIdEl: document.getElementById('js-page-hero'),
+  // pageHeroEL: document.querySelector('js-page-hero'),
+  // pageHeroByIdEl: document.getElementById('js-page-hero'),
 };
