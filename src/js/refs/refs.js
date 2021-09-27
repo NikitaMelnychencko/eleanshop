bdd057735c32234bae420eee63d382c790673e75
Fosa1990 <fosa1990@gmail.com>
2021-09-28 01:35:07 +0300

    add refs from page-hero
    deleted old refs

--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,6 +1,4 @@
 export default {
   bodyEl: document.querySelector('body'),
   mainEL: document.querySelector('main'),
-  // pageHeroEL: document.querySelector('js-page-hero'),
-  // pageHeroByIdEl: document.getElementById('js-page-hero'),
 };
