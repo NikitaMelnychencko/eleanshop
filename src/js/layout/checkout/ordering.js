eab5e9dee808721f09097d15b51b3aa39dc95e3a
Margarita555 <a_rita@ukr.net>
2021-09-27 12:52:58 +0300

    added the function that removes the card on clicking the close button

--- src/js/layout/checkout/ordering.js
+++ src/js/layout/checkout/ordering.js
@@ -8,32 +8,48 @@ refs.main.insertAdjacentHTML('beforeend', createCheckout);
 
 // const { incrementBtn, decrementBtn, orderingValueEl } = refs;
 
-const decrementBtn = document.querySelector('.ordering__btn--minus');
-const incrementBtn = document.querySelector('.ordering__btn--plus');
-const orderingValueEl = document.querySelector('.ordering__value');
-const priceValue = document.querySelector('.ordering__price');
-const totalPrice = document.querySelector('.ordering__totalprice');
+// const decrementBtn = document.querySelector('.ordering__btn--minus');
+// const incrementBtn = document.querySelector('.ordering__btn--plus');
+// const orderingValueEl = document.querySelector('.ordering__value');
+// const priceValue = document.querySelector('.ordering__price');
+// const totalPrice = document.querySelector('.ordering__totalprice');
+
 const cards = document.querySelector('.ordering__cards');
 
-cards.addEventListener('click', setQuantity);
+cards.addEventListener('click', setQuantityOrRemove);
 
  
-function setQuantity(e) {
-    if (e.target.nodeName !== 'BUTTON') {
-        return
-    }
-    
+function setQuantityOrRemove(e) {
+
     if (e.target.classList.contains('ordering__btn--plus')) {
-     let value = e.target.previousElementSibling.textContent;
-   
-        e.target.previousElementSibling.textContent = Number(value) + 1; 
-    }
-    else
-     if (e.target.classList.contains('ordering__btn--minus')) {
-     let value = e.target.nextElementSibling.textContent;
-        if(Number(value) > 0){
-         e.target.nextElementSibling.textContent = Number(value) - 1;  
-    }}
+        orderingIncrement(e);
+    }    
+     else if (e.target.classList.contains('ordering__btn--minus')) {
+        orderingDecrement(e)        
+    } 
+    else if (e.target.classList.contains('ordering__close')) {
+        removeOrderingCard(e)
+    }  
 }
 
+function orderingIncrement(e) {
+    let value = e.target.previousElementSibling.textContent;
 
+    e.target.previousElementSibling.textContent = Number(value) + 1;
+}
+
+function orderingDecrement(e) {
+         let value = e.target.nextElementSibling.textContent;
+        if(Number(value) > 0){
+         e.target.nextElementSibling.textContent = Number(value) - 1;  
+         }
+}
+
+function removeOrderingCard(e) {
+    const arr = e.currentTarget.children
+for (let i = 0; i <= arr.length; i += 1){
+    if (arr[i].contains(e.target)) {
+        e.currentTarget.removeChild(arr[i]);
+    }
+}
+}
