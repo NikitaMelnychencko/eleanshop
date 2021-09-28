8e79173d49bda5100822aa23aba0f9517b6bddf9
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-28 13:55:02 +0300

    add-create-markup
    add-create-markup

--- src/js/layout/fitting/sizeTable.js
+++ src/js/layout/fitting/sizeTable.js
@@ -4,5 +4,5 @@ import videoSlider_videoSliderCreate from './videoSlider'
 import informationAboutFitting_informationCreate from './informationAboutFitting'
 const menuEl = document.querySelector('main')
 const sizeTable_tableCreate = sizeTable_sizeTable();
-const fitting = sizeTable_markupAboutSizeTable( sizeTable_tableCreate)
+const fitting = sizeTable_markupAboutSizeTable( {sizeTable_tableCreate,videoSlider_videoSliderCreate,informationAboutFitting_informationCreate})
 menuEl.insertAdjacentHTML('beforeend',  fitting)
