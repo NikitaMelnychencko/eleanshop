5cf26e38b5905420869e238cad69eda4fdd4c7e9
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-28 04:35:31 +0300

    add input localStorage

--- src/js/layout/static/footer.js
+++ src/js/layout/static/footer.js
@@ -1,15 +1,9 @@
-// const refs = {
-//     plusLink: document.querySelectorAll('.open-menu'),
-//     minusLink: document.querySelectorAll('.footer__mobile-icon-menu-close'),
-//     openSubMenu: document.querySelector('.footer__mobile-list-dropdown'),
-//     isOpenIcon: document.querySelector('.footer__mobile-icon-menu-open'),
-//     removeIconPlus: document.querySelector('.footer__mobile-list-item'),
-
-// }
-
 import refs from '../../refs/refs.js'
 
-const { plusLink, minusLink, openSubMenu, isOpenIcon, removeIconPlus } = refs
+const { plusLink, minusLink, openSubMenu, isOpenIcon, removeIconPlus, iconMenu,
+    inputStorage } = refs
+console.log(inputStorage)
+
 // Функция раскрытия списка-меню
 
 plusLink.forEach((evt) => { /* Для каждого Li в списке */
@@ -20,28 +14,37 @@ plusLink.forEach((evt) => { /* Для каждого Li в списке */
         const arrow = sibling.querySelector('.footer__mobile-list-dropdown'); /* добираемся до вложенного списка меню */
         if (arrow) { /* проверяем есть ли вложенный списка */
             arrow.classList.toggle('js-dropdown'); /* обрабатываем его класс */
-
         }
     });
 });
-
-
-
 // input-storage
-// 
 
+
+/*удаление иконки плюс при раскрытии спика*/
 
 plusLink.forEach((evt) => { /* Для каждого Li списка*/
-    console.log(evt)
     const sibl = evt.firstElementChild; /* находим ближайший сестринский элемент */
-    console.log(sibl)
     evt.addEventListener('click', (el, evt) => { /* назначаем лишке обработчик события */
         el.preventDefault()/* что бы при клике не перегружалась страница*/
         const plus = sibl.querySelector('.footer__mobile-icon-menu'); /* добираемся до вложенного списка меню */
-        console.log(plus)
         if (plus) { /* проверяем есть ли вложенный список */
             plus.classList.toggle('is-open'); /* обрабатываем его класс */
         }
-        console.log(sibl)
     });
 });
+
+
+inputStorage.forEach((evt) => { /* Для каждого Li списка*/
+    evt.addEventListener('input', (evt) => { /* назначаем лишке обработчик события */
+        evt.preventDefault()/* что бы при клике не перегружалась страница*/
+        console.log(evt.target)
+        if (evt) {
+            localStorage.setItem('Input', 'js-footer-input')
+        }
+        // else {
+        //     localStorage.removeItem('Input')
+        // }
+    });
+});
+
+
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -4,6 +4,7 @@ export default {
     openSubMenu: document.querySelector('.footer__mobile-list-dropdown'),
     isOpenIcon: document.querySelector('.footer__mobile-icon-menu-open'),
     removeIconPlus: document.querySelectorAll('.footer__mobile-icon-position-relative'),
-
+    iconMenu: document.querySelector('.footer__mobile-icon-menu'),
+    inputStorage: document.querySelectorAll('.js-footer-input')
 
 }
