768b53b22fe4761ac51e58594d04eff185e741a0
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-28 03:31:59 +0300

    add-dataBase
    add-dataBase

--- src/js/layout/checkout/payment.js
+++ src/js/layout/checkout/payment.js
@@ -1,10 +1,12 @@
 import payment_payment from '../../../views/partials/checkout/payment.hbs'
 import payment_checkout from '../../../views/layouts/checkout.hbs'
+import method from '../../json/method.json'
 import refs from '../../refs/refs.js'
 import '../../../images/img/checkout/payment/buyers.jpg'
 import '../../../images/img/checkout/payment/buyers@2x.jpg'
 import '../../../images/img/checkout/payment/mirror_reflection.jpg'
 import '../../../images/img/checkout/payment/mirror_reflection@2x.jpg'
-const createPayment = payment_payment()
+const createPayment = payment_payment({ method })
+
 const createCheckout = payment_checkout({ createPayment })
 refs.main.insertAdjacentHTML('beforeend', createCheckout)
