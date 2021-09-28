519afaf65b9a6afd7e2abf8117624ca8450733ae
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-28 14:19:15 +0300

    fix

--- src/js/layout/fitting/sizeTable.js
+++ src/js/layout/fitting/sizeTable.js
@@ -2,7 +2,7 @@ import sizeTable_sizeTable from '../../../views/partials/fitting/sizeTable.hbs';
 import sizeTable_markupAboutSizeTable from '../../../views/layouts/fitting.hbs';
 import videoSlider_videoSliderCreate from './videoSlider'
 import informationAboutFitting_informationCreate from './informationAboutFitting'
-import'../../../images/img/elean47390-1+ 1.png'
+import'../../../images/img/elean47390.png'
 const menuEl = document.querySelector('main')
 const sizeTable_tableCreate = sizeTable_sizeTable();
 const fitting = sizeTable_markupAboutSizeTable( {sizeTable_tableCreate,videoSlider_videoSliderCreate,informationAboutFitting_informationCreate})
