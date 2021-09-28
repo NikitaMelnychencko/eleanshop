db7d7b6ebf862c0a1eb3c1c9be8dbd3e71545ad6
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-28 03:49:53 +0300

    edd eventlistener

--- src/js/layout/static/footer.js
+++ src/js/layout/static/footer.js
@@ -1,15 +1,47 @@
-const refs = {
-    plusLink: document.querySelector('.footer__mobile-list-item-plus'),
-    minusLink: document.querySelector('.footer__mobile-icon-menu-close'),
+// const refs = {
+//     plusLink: document.querySelectorAll('.open-menu'),
+//     minusLink: document.querySelectorAll('.footer__mobile-icon-menu-close'),
+//     openSubMenu: document.querySelector('.footer__mobile-list-dropdown'),
+//     isOpenIcon: document.querySelector('.footer__mobile-icon-menu-open'),
+//     removeIconPlus: document.querySelector('.footer__mobile-list-item'),
 
-}
+// }
 
-refs.plusLink.addEventListener('click', onClickPlusLink);
+import refs from '../../refs/refs.js'
 
-function onClickPlusLink(evt) {
-    evt.preventDefault();
-    console.log('Работает');
-    if (this.querySelector('.footer__mobile-list-dropdown')) {
-        this.classList.toggle('.js-dropdown');
-    }
-}
+const { plusLink, minusLink, openSubMenu, isOpenIcon, removeIconPlus } = refs
+// Функция раскрытия списка-меню
+
+plusLink.forEach((evt) => { /* Для каждого Li в списке */
+    const sibling = evt.nextElementSibling; /* находим ближайший сестринский элемент */
+    evt.addEventListener('click', (el, evt) => { /* назначаем лишке обработчик события */
+        el.preventDefault()/* что бы при клике не перегружалась страница*/
+        sibling.classList.toggle('js-dropdown'); /* добавляем/убираем класс у сестринского элемента */
+        const arrow = sibling.querySelector('.footer__mobile-list-dropdown'); /* добираемся до вложенного списка меню */
+        if (arrow) { /* проверяем есть ли вложенный списка */
+            arrow.classList.toggle('js-dropdown'); /* обрабатываем его класс */
+
+        }
+    });
+});
+
+
+
+// input-storage
+// 
+
+
+plusLink.forEach((evt) => { /* Для каждого Li списка*/
+    console.log(evt)
+    const sibl = evt.firstElementChild; /* находим ближайший сестринский элемент */
+    console.log(sibl)
+    evt.addEventListener('click', (el, evt) => { /* назначаем лишке обработчик события */
+        el.preventDefault()/* что бы при клике не перегружалась страница*/
+        const plus = sibl.querySelector('.footer__mobile-icon-menu'); /* добираемся до вложенного списка меню */
+        console.log(plus)
+        if (plus) { /* проверяем есть ли вложенный список */
+            plus.classList.toggle('is-open'); /* обрабатываем его класс */
+        }
+        console.log(sibl)
+    });
+});
---
+++ src/js/refs/refs.js
@@ -0,0 +1,9 @@
+export default {
+    plusLink: document.querySelectorAll('.open-menu'),
+    minusLink: document.querySelectorAll('.footer__mobile-icon-menu-close'),
+    openSubMenu: document.querySelector('.footer__mobile-list-dropdown'),
+    isOpenIcon: document.querySelector('.footer__mobile-icon-menu-open'),
+    removeIconPlus: document.querySelectorAll('.footer__mobile-icon-position-relative'),
+
+
+}
