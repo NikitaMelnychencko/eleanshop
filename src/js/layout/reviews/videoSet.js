d75e45faa0f13f68679f0cbf244eddb2b3207d83
LarysaTymoshenko <luda76@ukr.net>
2021-09-29 03:16:17 +0300

    style css

--- src/js/layout/reviews/videoSet.js
+++ src/js/layout/reviews/videoSet.js
@@ -2,7 +2,7 @@ import { refs } from '../../refs/refs.js'
 import set from '../../../../src/js/json/exampl.json';
 import setHbs from "../../../../src/views/partials/reviews/videoSet.hbs";
 import '../../../sass/layout/reviews/_videoSet.scss'
-
+import '../../../images/img/Reviews/poster/Black2V.jpg'
 
 
 const createSetList = (set) => {
