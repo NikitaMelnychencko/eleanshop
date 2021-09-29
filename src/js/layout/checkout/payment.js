4b5e4f8ac6c5fc1139e1dbe3ce93f080e48b5705
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-29 03:01:36 +0300

    add-showroom-method
    add-showroom-method

--- src/js/layout/checkout/payment.js
+++ src/js/layout/checkout/payment.js
@@ -6,7 +6,32 @@ import '../../../images/img/checkout/payment/buyers.jpg'
 import '../../../images/img/checkout/payment/buyers@2x.jpg'
 import '../../../images/img/checkout/payment/mirror_reflection.jpg'
 import '../../../images/img/checkout/payment/mirror_reflection@2x.jpg'
-const createPayment = payment_payment({ method })
+import '../../../images/svg/murkup.svg'
+import '../../../images/svg/murkup_arow.svg'
 
+const createPayment = payment_payment({ method })
 const createCheckout = payment_checkout({ createPayment })
 refs.main.insertAdjacentHTML('beforeend', createCheckout)
+
+//const listDay = document.querySelector('.showroom-list')
+const formDay = document.querySelector('.showroom-method')
+const delivery = document.querySelector('.delivery-method')
+delivery.addEventListener('click', event => {
+  if (event.target.nodeName !== "INPUT") {
+    return
+  }
+  console.dir(event.target.checked);
+  if (event.target.value=== "Showroom"&&event.target.checked=== true) {
+    formDay.classList.add('showroom-method--hide')
+  } else {
+    formDay.classList.remove('showroom-method--hide')
+  }
+})
+formDay.addEventListener('click', event => {
+  
+  const lists = document.querySelector(`.${event.target.id}`)
+  lists.classList.toggle("showroom-list--hide")
+  document.body.classList.toggle("extra")
+  
+  
+})
