1a092e4b47512dacdffdc1340ddc12d05b18e706
Serhii Sokolov <68744408+Falconoff@users.noreply.github.com>
2021-09-29 14:40:09 +0300

    Update tryOnModelsModal.js

--- src/js/layout/product/tryOnModelsModal.js
+++ src/js/layout/product/tryOnModelsModal.js
@@ -3,10 +3,10 @@
 import tryOnModelsModal from '../../../views/components/tryOnModelsModal.hbs';
 import orderForm from '../../../views/components/orderForm.hbs';
 
-// create content for Pre-order Modal which placed at Body (for example)
+// create content for Try-One Modal which placed at Body (for example)
 const body = document.querySelector('body');
 body.insertAdjacentHTML('beforeend', tryOnModelsModal());
 
-// add Order Form as second part of Pre-order Modal content
+// add Order Form as second part of Try-One Modal content
 const tryOnEl = document.querySelector('.try-on');
 tryOnEl.insertAdjacentHTML('beforeend', orderForm());
