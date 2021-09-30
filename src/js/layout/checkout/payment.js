222427aac76e5f8b93b059c7f37c75a5874d45cc
NikitaMelnychencko <hunter6059@gmail.com>
2021-10-01 02:55:02 +0300

    add-local
    add-local

--- src/js/layout/checkout/payment.js
+++ src/js/layout/checkout/payment.js
@@ -12,12 +12,50 @@ import '../../../images/svg/murkup_arow.svg'
 const createPayment = payment_payment({ method })
 const createCheckout = payment_checkout({ createPayment })
 refs.mainEL.insertAdjacentHTML('beforeend', createCheckout)
+const formDay =document.querySelector('.showroom-method')
+const delivery = document.querySelector('.delivery-method')
+const payment = document.querySelector('.payment')
+const infoEL = document.querySelector('.basic-information')
+const arrInputInfo = infoEL.querySelectorAll('input')
+const textareaInfo = infoEL.querySelectorAll('textarea')
+
+delivery.addEventListener('click', event => {
+  if (event.target.nodeName !== "INPUT") {
+    return
+  }
+  if (event.target.value=== "Showroom"&&event.target.checked=== true) {
+    formDay.classList.add('showroom-method--hide')
+  } else {
+    formDay.classList.remove('showroom-method--hide')
+        
+  }
+  if (event.target.checked===true) {
+    localStorage.setItem(`${event.target.name}`,`${event.target.value}`)
+  }
+})
+payment.addEventListener('click', event => {
+  if (event.target.nodeName !== "INPUT") {
+    return
+  }
+  if (event.target.checked===true) {
+    localStorage.setItem(`${event.target.name}`,`${event.target.value}`)
+  }
+})
+arrInputInfo.forEach(elem => {
+  elem .addEventListener('blur', event => {
+    localStorage.setItem(`${event.target.name}`,`${event.target.value}`)
+  })
+})
+textareaInfo.addEventListener('click', event => {
+  localStorage.setItem(`${event.target.name}`,`${event.target.value}`)
+
+})
+
 
 class modalData{
   constructor({idInput,idList}) {
     this._refs = this._getRefs(idInput, idList)
 
-    this._addEventDelivery()
     this._addEventInput()
     this._addEventList()
 
@@ -26,25 +64,11 @@ class modalData{
     const refs = {
       input: document.getElementById(`${idInput}`),
       list: document.getElementById(`${idList}`),
-      formDay: document.querySelector('.showroom-method'),
-      delivery : document.querySelector('.delivery-method')
+     
     }
     return refs
   }
-  _addEventDelivery() {
-    this._refs.delivery.addEventListener('click', event => {
-      if (event.target.nodeName !== "INPUT") {
-        return
-      }
-      if (event.target.value=== "Showroom"&&event.target.checked=== true) {
-        this._refs.formDay.classList.add('showroom-method--hide')
-      } else {
-        this._refs.formDay.classList.remove('showroom-method--hide')
-        this._refs.input.value = ""
-      }
-    })
-    
-  }
+
   _addEventInput() {
     this._refs.input.addEventListener('click', event => {
       this._refs.list.classList.toggle("showroom-list--hide")
@@ -57,6 +81,7 @@ class modalData{
     this._refs.list.addEventListener('click', event => {
       this._refs.input.value = event.target.innerText
       this._refs.list.classList.toggle("showroom-list--hide")
+      localStorage.setItem(`${this._refs.input.name}`,`${event.target.innerText}`)
      })
   }
 
@@ -74,6 +99,3 @@ const inputDay = new modalData({
 })
 
 
-const deliveryMethod = document.querySelector('.delivery-method')
-
-deliveryMethod.addEventListener('click')
