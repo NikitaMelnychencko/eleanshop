30332d46b4cdfa79e8b837249548503e4ed4f6c8
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-30 12:39:26 +0300

    fix-condig

--- src/js/layout/home/aboutTheBrand.js
+++ src/js/layout/home/aboutTheBrand.js
@@ -1,8 +1,10 @@
 import aboutTheBrand_aboutBrandTempl from '../../../views/partials/home/aboutTheBrand.hbs';
 
+import refs from '../../refs/refs';
+    
 const { mainEL } = refs;
-
-mainEL.insertAdjacentHTML('beforeend', aboutTheBrand_aboutBrandTempl());
+const aboutTheBrand_parsing = aboutTheBrand_aboutBrandTempl();
+mainEL.insertAdjacentHTML('beforeend', aboutTheBrand_parsing);
 const homeAboutBrandLink = document.querySelector('.aboutBrand__link');
 homeAboutBrandLink.addEventListener('click', onAboutBrandLinkClick);
 function onAboutBrandLinkClick(event){
