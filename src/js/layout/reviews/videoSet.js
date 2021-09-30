23c4f21c3b2357595ed4809aa6021a1924fb101e
LarysaTymoshenko <luda76@ukr.net>
2021-09-30 10:42:47 +0300

    1042

--- src/js/layout/reviews/videoSet.js
+++ src/js/layout/reviews/videoSet.js
@@ -4,7 +4,7 @@ import setHbs from "../../../../src/views/partials/reviews/videoSet.hbs";
 import '../../../sass/layout/reviews/_videoSet.scss'
 import '../../../images/img/Reviews/poster/Black2V.jpg'
 
-
+// 1. Содание разметки сета
 const createSetList = (set) => {
     return setHbs(set);
 }
@@ -13,3 +13,38 @@ const createSetList = (set) => {
 const setListMarkup = createSetList(set);
 
 refs.setList.insertAdjacentHTML('beforeend',setListMarkup);
+
+
+// 2. Создание click
+setList.addEventListener('click', onOpenModal);
+BtnEl.addEventListener('click', onCloseModal);
+overlay.addEventListener('click', onCloseModal);
+
+
+  
+  //3. работа модального окна
+function onOpenModal(evt) {
+ console.log(evt);
+// evt.preventDefault();
+//     if (evt.target.nodeName !== 'IMG') {
+//     return;
+}
+
+// function replaceAtribute(src, alt ) {
+//   videoEl.src = src;
+//   videoEl.alt = alt;
+// }
+
+// function onCloseModal(e) {
+//    e.videobox.classList.remove('is-open')
+//     replaceAtribute(" ", " ")
+// };
+// // 4. Закрытие модалки с помощью "ESC"
+// window.addEventListener('keydown', onCloseModalEsc);
+
+// function onCloseModalEsc(evt) {
+//   if (evt.code === 'Escape') {
+//     onCloseModal()
+//   };
+  
+// }
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,4 +1,9 @@
 export const refs = {
   setList: document.querySelector('.js-videoset'),
-
+  
+ 
+  videobox: document.querySelector('.js-videobox'),
+  overlay: document.querySelector('.videobox__overlay'),
+  videoEl: document.querySelector('.video-set__video'),
+  BtnEl: document.querySelector('[data-action="close-videobox"]')
 }
