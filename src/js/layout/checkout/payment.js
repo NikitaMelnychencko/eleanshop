edf9224ec75674701e9c3751d01a1e36e9bc0117
NikitaMelnychencko <hunter6059@gmail.com>
2021-09-30 03:50:53 +0300

    modalData
    add class modalData

--- src/js/layout/checkout/payment.js
+++ src/js/layout/checkout/payment.js
@@ -13,25 +13,61 @@ const createPayment = payment_payment({ method })
 const createCheckout = payment_checkout({ createPayment })
 refs.mainEL.insertAdjacentHTML('beforeend', createCheckout)
 
-//const listDay = document.querySelector('.showroom-list')
-const formDay = document.querySelector('.showroom-method')
-const delivery = document.querySelector('.delivery-method')
-delivery.addEventListener('click', event => {
-  if (event.target.nodeName !== "INPUT") {
-    return
+class modalData{
+  constructor({idInput,idList}) {
+    this._refs = this._getRefs(idInput, idList)
+
+    this._addEventDelivery()
+    this._addEventInput()
+    this._addEventList()
+
   }
-  console.dir(event.target.checked);
-  if (event.target.value=== "Showroom"&&event.target.checked=== true) {
-    formDay.classList.add('showroom-method--hide')
-  } else {
-    formDay.classList.remove('showroom-method--hide')
+  _getRefs(idInput,idList) {
+    const refs = {
+      input: document.getElementById(`${idInput}`),
+      list: document.getElementById(`${idList}`),
+      formDay: document.querySelector('.showroom-method'),
+      delivery : document.querySelector('.delivery-method')
+    }
+    return refs
   }
+  _addEventDelivery() {
+    this._refs.delivery.addEventListener('click', event => {
+      if (event.target.nodeName !== "INPUT") {
+        return
+      }
+      if (event.target.value=== "Showroom"&&event.target.checked=== true) {
+        this._refs.formDay.classList.add('showroom-method--hide')
+      } else {
+        this._refs.formDay.classList.remove('showroom-method--hide')
+        this._refs.input.value = ""
+      }
+    })
+  }
+  _addEventInput() {
+    this._refs.input.addEventListener('click', event => {
+      this._refs.list.classList.toggle("showroom-list--hide")
+      document.body.classList.toggle("extra")
+      
+    })
+
+  }
+  _addEventList() {
+    this._refs.list.addEventListener('click', event => {
+      this._refs.input.value = event.target.innerText
+      this._refs.list.classList.toggle("showroom-list--hide")
+     })
+  }
+
+}
+
+const inputTime = new modalData({
+  idInput: "js-time",
+  idList: "time-list",
+  
 })
-formDay.addEventListener('click', event => {
-  
-  const lists = document.querySelector(`.${event.target.id}`)
-  lists.classList.toggle("showroom-list--hide")
-  document.body.classList.toggle("extra")
-  
+const inputDay = new modalData({
+  idInput: "js-day",
+  idList: "day-list",
   
 })
