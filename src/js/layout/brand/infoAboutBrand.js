88bf4c77300dc453efd6b6b9fbf9e0d479ce9f68
VladVinichenko <vinnyxxxl925@gmail.com>
2021-09-30 14:04:43 +0300

    Brand-page complete
    v1

--- src/js/layout/brand/infoAboutBrand.js
+++ src/js/layout/brand/infoAboutBrand.js
@@ -1,5 +1,8 @@
 import brand_info from '../../../views/layouts/brand.hbs';
 import form_brand from '../../..//views/partials/brand/formFittingInShowroom.hbs';
+import brand_our_advantages from '../../..//views/partials/brand/ourAdvantages.hbs';
+import card_brand_our_advantages from '../../..//views/partials/brand/cardBrandOurAdvantages.hbs';
+
 import refs from '../../refs/refs';
 import '../../../images/img/brand/elena-des.jpg';
 import '../../../images/img/brand/elena-des@2x.jpg';
@@ -9,97 +12,33 @@ import '../../../images/img/brand/two-an-des.jpg';
 import '../../../images/img/brand/two-an-des@2x.jpg';
 import '../../../images/img/brand/two-an-mob.jpg';
 import '../../../images/img/brand/two-an-mob@2x.jpg';
-import '../../../images/svg/exampl.svg'
-// import 'https://www.youtube.com/iframe_api';
+import '../../../images/svg/exampl.svg';
 
-// const player = document.querySelector('player')
+const brandOurAdvantagesCard = [
+  {
+    url: './images/exampl.svg#icon-needle',
+    text: 'Уровень пошива приравнивается к индивидуальному;',
+  },
+  {
+    url: './images/exampl.svg#icon-mannequin',
+    text: 'Создаем лекала для костюмов, которые подчеркивают достоинства и скрывают недостатки фигуры;',
+  },
+  {
+    url: './images/exampl.svg#icon-flower',
+    text: 'Качественное исполнение: ровные отстрочки, красивая обработка – вы можете носить вещи даже изнаночной стороной;',
+  },
+  {
+    url: './images/exampl.svg#icon-sewing-machine',
+    text: 'Используем современное оборудование и качественные материалы для изготовления изделий;',
+  },
+  {
+    url: './images/exampl.svg#icon-cuboc',
+    text: 'Наши швеи занимаются пошивом и конструированием одежды более 20 лет. За это время их навыки доведены до идеала... ',
+  },
+];
+
+const cardBrandOurAdvantages = card_brand_our_advantages(brandOurAdvantagesCard);
 const formBrand = form_brand();
-const brandInfo = brand_info({formBrand});
-
-
+const brandOurAdvantages = brand_our_advantages({ formBrand, cardBrandOurAdvantages });
+const brandInfo = brand_info({ brandOurAdvantages });
 refs.brand.insertAdjacentHTML('beforeend', brandInfo);
-      // // 2. This code loads the IFrame Player API code asynchronously.
-      // var tag = document.createElement('script');
-
-      // tag.src = "https://www.youtube.com/iframe_api";
-      // var firstScriptTag = document.getElementsByTagName('script')[0];
-      // firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // // 3. This function creates an <iframe> (and YouTube player)
-      // //    after the API code downloads.
-      // var player;
-      // function onYouTubeIframeAPIReady() {
-      //   player = new YT.Player('player', {
-      //     height: '360',
-      //     width: '640',
-      //     videoId: 'https://www.youtube.com/watch?v=JHAof88rAuI',
-      //     events: {
-      //       'onReady': onPlayerReady,
-      //       'onStateChange': onPlayerStateChange
-      //     }
-      //   });
-      // }
-
-      // // 4. The API will call this function when the video player is ready.
-      // function onPlayerReady(event) {
-      //   event.target.playVideo();
-      // }
-
-      // // 5. The API calls this function when the player's state changes.
-      // //    The function indicates that when playing a video (state=1),
-      // //    the player should play for six seconds and then stop.
-      // var done = false;
-      // function onPlayerStateChange(event) {
-      //   if (event.data == YT.PlayerState.PLAYING && !done) {
-      //     setTimeout(stopVideo, 6000);
-      //     done = true;
-      //   }
-      // }
-      // function stopVideo() {
-      //   player.stopVideo();
-      // }
-
-
-//       const YTPlayer = require('yt-player')
-// const player = new YTPlayer('#player')
- 
-// player.load('GKSRyLdjsPA')
-// player.setVolume(100)
- 
-// player.on('playing', () => {
-//   console.log(player.getDuration()) // => 351.521
-// })
-
-
-
-const YTPlayer = require('yt-player')
-// const player = new YTPlayer('#player')
-
-// player.load('fpyu_GgmpQc')
-// player.setVolume(100)
-
-// player.on('playing', () => {
-//   console.log(player.getDuration()) // => 351.521
-// })
-
-const refBrand = {
-  player: document.querySelector('#player')
-}
-
-const opts = {
-  autoplay: true,
-width: 500,
-height: 500,
-videoId: 'JHAof88rAuI',
-// annotations: false,
-}
-
-const yplayer = new YTPlayer(refBrand.player, opts);
-
-yplayer.load('GKSRyLdjsPA')
-yplayer.setVolume(100)
-
-console.log(yplayer);
-
-refBrand.player.insertAdjacentHTML('beforeend', yplayer);
-
