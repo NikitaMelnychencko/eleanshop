8762757f971029ed0459ce650a42df11953b713f
LarysaTymoshenko <luda76@ukr.net>
2021-09-30 23:02:08 +0300

    2302

--- src/js/layout/reviews/videoSet.js
+++ src/js/layout/reviews/videoSet.js
@@ -4,6 +4,8 @@ import setHbs from "../../../../src/views/partials/reviews/videoSet.hbs";
 import '../../../sass/layout/reviews/_videoSet.scss'
 import '../../../images/img/Reviews/poster/Black2V.jpg'
 
+
+const { setList, videobox, overlay, videoEl, BtnEl } = refs;
 // 1. Содание разметки сета
 const createSetList = (set) => {
     return setHbs(set);
@@ -17,34 +19,35 @@ refs.setList.insertAdjacentHTML('beforeend',setListMarkup);
 
 // 2. Создание click
 setList.addEventListener('click', onOpenModal);
-BtnEl.addEventListener('click', onCloseModal);
+ BtnEl.addEventListener('click', onCloseModal);
 overlay.addEventListener('click', onCloseModal);
 
 
   
   //3. работа модального окна
 function onOpenModal(evt) {
- console.log(evt);
-// evt.preventDefault();
-//     if (evt.target.nodeName !== 'IMG') {
-//     return;
+    console.log(evt);
+    // evt.preventDefault();
+    // if (evt.target.nodeName !== 'VIDEO') {
+    //     return;
+    // }
 }
 
-// function replaceAtribute(src, alt ) {
+// function replaceAtribute(src ) {
 //   videoEl.src = src;
-//   videoEl.alt = alt;
+  
 // }
 
 // function onCloseModal(e) {
 //    e.videobox.classList.remove('is-open')
 //     replaceAtribute(" ", " ")
 // };
-// // 4. Закрытие модалки с помощью "ESC"
-// window.addEventListener('keydown', onCloseModalEsc);
+// 4. Закрытие модалки с помощью "ESC"
+window.addEventListener('keydown', onCloseModalEsc);
 
-// function onCloseModalEsc(evt) {
-//   if (evt.code === 'Escape') {
-//     onCloseModal()
-//   };
+function onCloseModalEsc(evt) {
+  if (evt.code === 'Escape') {
+    onCloseModal()
+  };
   
-// }
+}
--- src/js/refs/refs.js
+++ src/js/refs/refs.js
@@ -1,9 +1,10 @@
 export const refs = {
   setList: document.querySelector('.js-videoset'),
   
- 
   videobox: document.querySelector('.js-videobox'),
   overlay: document.querySelector('.videobox__overlay'),
   videoEl: document.querySelector('.video-set__video'),
   BtnEl: document.querySelector('[data-action="close-videobox"]')
 }
+
+const {setList,videobox,overlay,videoEl,BtnEl}= refs
