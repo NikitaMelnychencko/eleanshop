dae43cad2970863dd37182b645b2c4277729b66d
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2021-09-30 09:30:18 +0300

    size table

--- src/js/layout/fitting/informationAboutFitting.js
+++ src/js/layout/fitting/informationAboutFitting.js
@@ -1,6 +1,6 @@
 import informationAboutFitting from '../../../views/partials/fitting/informationAboutFitting.hbs';
 import informationAboutFitting_markupAboutSizeTable from '../../../views/layouts/fitting.hbs';
-import '../../../images/img/elean47390.png'
+/* import '../../../images/img/elean47390.png' */
 const menuEl = document.querySelector('main')
 const informationAboutFitting_informationCreate = informationAboutFitting();
 export default informationAboutFitting_informationCreate
