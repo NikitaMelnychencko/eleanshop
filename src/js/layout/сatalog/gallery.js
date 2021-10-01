00fec933b7ddadf7f947f73558b97ab63ba5f40c
IraMcsimova <ilisay777@gmail.com>
2021-10-01 22:06:29 +0300

    catalog-gallery
    filter-animation

--- src/js/layout/сatalog/gallery.js
+++ src/js/layout/сatalog/gallery.js
@@ -1,7 +1,7 @@
 import gallery from '../../../views/partials/сatalog/gallery.hbs';
-import filter from '../../../views/partials/сatalog/filter.hbs';
+
 import catalog from '../../json/catalog.json';
-import filterLib from '../../json/filterLib.json';
+
 import '../../../images/svg/catalog/catalog.svg';
 
 const catalogItems = document.querySelector('.js-catalog');
@@ -12,11 +12,3 @@ function catalogCreating() {
 }
 
 catalogCreating();
-
-const filterItem = document.querySelector('#filter');
-
-function filterCreating() {
-  const filterListMakeup = filterLib.map(elem => filter(elem)).join('');
-  filterItem.innerHTML = filterListMakeup;
-}
-filterCreating();
