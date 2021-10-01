15e642eceb9ab3e3cb7c6a84300ed251fe60faea
VladVinichenko <vinnyxxxl925@gmail.com>
2021-10-01 05:34:19 +0300

    z

--- src/js/layout/contact/contact.js
+++ src/js/layout/contact/contact.js
@@ -1,8 +1,13 @@
 import contactsTemplate from '../../../views/layouts/contact.hbs';
-import { contactsData } from '../../../../app';
+import contact_map from '../../../views/partials/contact/map.hbs';
+
+import contactsData from '../../../../app';
 import refs from '../../refs/refs';
 const { main } = refs;
 
+const contactsMap = contact_map();
 const contactsMarkup = contactsTemplate(contactsData);
 
 main.insertAdjacentHTML('beforeend', contactsMarkup);
+
+console.log(contactsMap);
