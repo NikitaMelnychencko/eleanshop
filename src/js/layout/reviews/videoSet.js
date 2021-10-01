0a5a82f8734b1d23d0ec8fdf98524d0caa1e47a6
LarysaTymoshenko <luda76@ukr.net>
2021-10-01 18:53:52 +0300

    1853

--- src/js/layout/reviews/videoSet.js
+++ src/js/layout/reviews/videoSet.js
@@ -5,7 +5,7 @@ import '../../../sass/layout/reviews/_videoSet.scss'
 import '../../../images/img/Reviews/poster/Black2V.jpg'
 
 
-const { setList, videobox, overlay, videoEl, BtnEl } = refs;
+// const { setList, videobox, overlay, videoEl, BtnEl } = refs;
 // 1. Содание разметки сета
 const createSetList = (set) => {
     return setHbs(set);
@@ -24,31 +24,31 @@ overlay.addEventListener('click', onCloseModal);
 
 
   
-  //3. работа модального окна
-function onOpenModal(evt) {
-    console.log(evt);
-    // evt.preventDefault();
-    // if (evt.target.nodeName !== 'VIDEO') {
-    //     return;
-    // }
-}
+//   //3. работа модального окна
+// function onOpenModal(evt) {
+//     console.log(evt);
+//     // evt.preventDefault();
+//     // if (evt.target.nodeName !== 'VIDEO') {
+//     //     return;
+//     // }
+// }
 
-function replaceAtribute(src ) {
-  videoEl.src = src;
+// function replaceAtribute(src ) {
+//   videoEl.src = src;
   
-}
+// }
 
-function onCloseModal(e) {
-   e.videobox.classList.remove('is-open')
-    replaceAtribute(" ")
-};
+// function onCloseModal(e) {
+//    e.videobox.classList.remove('is-open')
+//     replaceAtribute(" ")
+// };
 
-// 4. Закрытие модалки с помощью "ESC"
-window.addEventListener('keydown', onCloseModalEsc);
+// // 4. Закрытие модалки с помощью "ESC"
+// window.addEventListener('keydown', onCloseModalEsc);
 
-function onCloseModalEsc(evt) {
-  if (evt.code === 'Escape') {
-    onCloseModal()
-  };
+// function onCloseModalEsc(evt) {
+//   if (evt.code === 'Escape') {
+//     onCloseModal()
+//   };
   
-}
+// }
