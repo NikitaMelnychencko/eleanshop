8e39010c977c545cb944f50e8006fddf69daee6f
IrinaShushkevych <IraOlijnyk@gmail.com>
2021-10-03 02:10:02 +0300

    correct json

--- src/js/layout/product/recomendationsCategory.js
+++ src/js/layout/product/recomendationsCategory.js
@@ -3,7 +3,8 @@ require('../../slick/slick.min.js');
 
 import cardsMarkup from '../../../views/partials/product/recomendationsCategory.hbs';
 import listCards from '../../../views/partials/product/productListFromCatalog.hbs';
-import cards from '../../json/recomendationCategory.json';
+// import cards from '../../json/recomendationCategory.json';
+import cards from '../../json/catalog.json';
 
 export default class RecomendationsCategory {
   constructor({ root, typeInsert, data = cards, countsCard = 4, buttonPagination = true }) {
