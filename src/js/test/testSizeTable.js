d511fd3f260f5519f7f837054a613a1f0736a4cc
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-10-03 16:35:52 +0300

    update

---
+++ src/js/test/testSizeTable.js
@@ -0,0 +1,10 @@
+import sizeTable from '../layout/fitting/sizeTable.js';
+
+const body = document.querySelector('body');
+body.insertAdjacentHTML('beforebegin', '<button data-modal-open>!!!!!!!BUTTON ON MODAL Size Chose !!!!!!</button>');
+
+const { onBtnClick} = sizeTable;
+
+const btnRef = document.querySelector('[data-modal-open]');
+
+btnRef.addEventListener('click', onBtnClick);
