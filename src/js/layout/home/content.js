c4b07fb569eb95a8572a349c67680f4922fb7bf9
DeadNord <eosipopo@gmail.com>
2021-10-03 14:45:26 +0300

    grid-fix + js

--- src/js/layout/home/content.js
+++ src/js/layout/home/content.js
@@ -1,58 +1,21 @@
 // Import
 import model from "../../../views/partials/home/content.hbs";
 import content from "../../json/home/content.json";
+import refs from "../../refs/refs";
 
-//Import Images
-// Mobile
-import "../../../images/img/home/content/mobile/img-mob_1.jpg"
-import "../../../images/img/home/content/mobile/img-mob_1@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_2.jpg"
-import "../../../images/img/home/content/mobile/img-mob_2@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_3.jpg"
-import "../../../images/img/home/content/mobile/img-mob_3@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_4.jpg"
-import "../../../images/img/home/content/mobile/img-mob_4@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_5.jpg"
-import "../../../images/img/home/content/mobile/img-mob_5@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_6.jpg"
-import "../../../images/img/home/content/mobile/img-mob_6@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_7.jpg"
-import "../../../images/img/home/content/mobile/img-mob_7@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_8.jpg"
-import "../../../images/img/home/content/mobile/img-mob_8@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_9.jpg"
-import "../../../images/img/home/content/mobile/img-mob_9@2x.jpg"
-import "../../../images/img/home/content/mobile/img-mob_10.jpg"
-import "../../../images/img/home/content/mobile/img-mob_10@2x.jpg"
-// Desktop
-import "../../../images/img/home/content/desk/img_1.jpg"
-import "../../../images/img/home/content/desk/img_1@2x.jpg"
-import "../../../images/img/home/content/desk/img_2.jpg"
-import "../../../images/img/home/content/desk/img_2@2x.jpg"
-import "../../../images/img/home/content/desk/img_3.jpg"
-import "../../../images/img/home/content/desk/img_3@2x.jpg"
-import "../../../images/img/home/content/desk/img_4.jpg"
-import "../../../images/img/home/content/desk/img_4@2x.jpg"
-import "../../../images/img/home/content/desk/img_5.jpg"
-import "../../../images/img/home/content/desk/img_5@2x.jpg"
-import "../../../images/img/home/content/desk/img_6.jpg"
-import "../../../images/img/home/content/desk/img_6@2x.jpg"
-import "../../../images/img/home/content/desk/img_7.jpg"
-import "../../../images/img/home/content/desk/img_7@2x.jpg"
-import "../../../images/img/home/content/desk/img_8.jpg"
-import "../../../images/img/home/content/desk/img_8@2x.jpg"
-import "../../../images/img/home/content/desk/img_9.jpg"
-import "../../../images/img/home/content/desk/img_9@2x.jpg"
-import "../../../images/img/home/content/desk/img_10.jpg"
-import "../../../images/img/home/content/desk/img_10@2x.jpg"
-
-
-// Links
-const refs = {
-    main: document.querySelector("main"),
-    cards: model(content)
-};
 
 
 // Cards Insert
-refs.main.insertAdjacentHTML("beforeend", refs.cards);
+const cards = model(content);
+refs.main.insertAdjacentHTML("beforeend", cards);
+
+// Links
+const ref = {
+    list: document.querySelector(".content__list"),
+};
+
+// Target
+localStorage.removeItem("content");
+ref.list.addEventListener("click", e => {
+    localStorage.setItem("content", `${e.target.id}`)
+});
