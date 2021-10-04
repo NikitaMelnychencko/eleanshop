93ed54dcbc0a948376745d05cf5ea26f123489a9
Volodymyr Tyshchenko <volodymyr.tyshchenko@gmail.com>
2021-10-04 21:27:02 +0300

    js beta realization

--- src/js/layout/favorites/favorites.js
+++ src/js/layout/favorites/favorites.js
@@ -11,20 +11,55 @@ class Favorites {
   }
 
   init() {
-    data = document.querySelector('.favorites__data');
-    sendEMail = document.querySelector('.favorites__sand-on-email--button');
-    data.addEventListener('click', onButtonsClick);
-    sendEMail.addEventListener('click', onButtonSendEMailClick);
+    dataRef = document.querySelector('.favorites__data');
+    sendEMailRef = document.querySelector('.favorites__sand-on-email--button');
+    dataRef.addEventListener('click', onButtonsClick);
+    sendEMailRef.addEventListener('click', onButtonSendEMailClick);
   }
 
   onButtonsClick(event) {
     if (event.target.classList.includes('favorites__button-buy')) {
-      // bin
+      let id = event.target.paewnt.paremt.id;
+      const ulRef = document.querySelector('.favorites__data');
+      let i = 0;
+      for (const el of ulRef.childNodes) {
+        if (el.id !== id) {
+          i += 1;
+        } else {
+          break;
+        }
+      }
+      elem = {};
+      elem.id = this.data['fav'][i].id;
+      elem.name = this.data['fav'][i].name;
+      let imgs = '';
+      if (window.screen.size < 1379) {
+        imgs = this.data['fav'][i].image['srcset-mobile'];
+      } else {
+        imgs = this.data['fav'][i].image.srcset;
+      }
+      imgs = imgs.split(',').split(' ');
+      elem.img = imgs[0];
+      elem.img2 = imgs[2];
+      elem.price = this.data['fav'][i].price;
+      elem.sizeSelected = this.data['fav'][i].size;
+      elem.colorSelected = this.data['fav'][i].color;
+      elem.circleSelected = '';
+      elem.count = 1;
+
+      dataBin = localStorage.getItem('orderingData');
+      if (dataBin !== null) {
+        dataBin = JSON.parse(dataBin);
+        dataBin = [...dataBin, elem];
+      } else {
+        dataBin = [elem];
+      }
+      localStorage.setItem('orderingData', JSON.stringify(dataBin));
     } else if (event.target.classList.includes('favorites__button-delete--icon')) {
       data = localStorage.getItem('favorites');
       if (data !== null) {
-        id = event.target.paewnt.paremt.paremt.id;
-        data = data.JSON.parse(data);
+        let id = event.target.paewnt.paremt.paremt.id;
+        data = JSON.parse(data);
         this.data['fav'] = data['fav'].forEach(element => element.id != id);
       }
       localStorage.setItem('favorites', JSON.stringify(this.data));
