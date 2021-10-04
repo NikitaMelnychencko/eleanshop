a11dc2bb7d125a0b987aadd922e4ae18e61aab01
Nataliia Salamatina <nataliia.salamatina@gmail.com>
2021-10-04 10:24:39 +0300

    finnal commit

--- src/js/layout/fitting/videoSlider.js
+++ src/js/layout/fitting/videoSlider.js
@@ -1,7 +1,39 @@
 import videoSlider from '../../../views/partials/fitting/videoSlider.hbs';
 import videoSlider_markupVideo from '../../../views/layouts/fitting.hbs';
+import '../../../images/svg/arrow-left.svg'
+import '../../../images/svg/arrow-right.svg'
+
 
 const menuEl = document.querySelector('main')
 const videoSlider_videoSliderCreate = videoSlider();
 
 export default videoSlider_videoSliderCreate
+
+
+//SLIDER SETTINGS
+$(document).ready(function() {
+    $('.slider').slick({
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true,
+        autoPlaySpeed: 2000,
+        pauseFocus: true,
+        pauseHover: true,
+        pauseOnDotsHover: true,
+        swipe: true,
+        
+        
+      //  mobileFirst: true,
+        responsive: [
+            {
+                breakpoint: 320,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    swipe: true
+                }
+            }
+        ]
+    });
+});
