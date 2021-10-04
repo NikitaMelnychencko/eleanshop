b5618eccefcde23305541dc5d546cef285695554
NikitaMelnychencko <hunter6059@gmail.com>
2021-10-04 18:46:06 +0300

    +

--- src/js/layout/fitting/sizeTable.js
+++ src/js/layout/fitting/sizeTable.js
@@ -1,8 +1,8 @@
 import sizeTable_sizeTable from '../../../views/partials/fitting/sizeTable.hbs';
 import sizeTable_markupAboutSizeTable from '../../../views/layouts/fitting.hbs';
-import videoSlider_videoSliderCreate from './videoSlider'
-import informationAboutFitting_informationCreate from './informationAboutFitting'
-import'../../../images/img/elean47390.png'
+import videoSlider_videoSliderCreate from './videoSlider.js'
+import informationAboutFitting_informationCreate from './informationAboutFitting.js'
+import '../../../images/img/elean47390.jpg'
 const menuEl = document.querySelector('main')
 const sizeTable_tableCreate = sizeTable_sizeTable();
 const fitting = sizeTable_markupAboutSizeTable( {sizeTable_tableCreate,videoSlider_videoSliderCreate,informationAboutFitting_informationCreate})
