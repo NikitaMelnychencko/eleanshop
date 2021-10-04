ae814a55d1c1d6582f4313da1a45a7cd5349cb1e
VladVinichenko <vinnyxxxl925@gmail.com>
2021-10-05 00:04:40 +0300

    zzz4

--- src/js/layout/reviews/registrationFormForFitting.js
+++ src/js/layout/reviews/registrationFormForFitting.js
@@ -2,96 +2,65 @@ import form_reviews from '../../../views/partials/reviews/formReviews.hbs';
 export const formReviewsMarkUp = form_reviews();
 
 export function formReviews() {
-  const formFitting = {
-    box: document.querySelectorAll('.form-fitting-showroom'),
+  const formReviewsAskQuestion = {
+    box: document.querySelectorAll('.form-reviews'),
     name: document.querySelectorAll('[name="name"]'),
     tel: document.querySelectorAll('[name="tel"]'),
-    date: document.querySelectorAll('[name="date"]'),
+    email: document.querySelectorAll('[name="email"]'),
     comment: document.querySelectorAll('[name="comment"]'),
-    form: document.querySelectorAll('.form-fitting-showroom__form'),
+    form: document.querySelectorAll('.form-reviews__form'),
     formInputs: document.querySelectorAll('input'),
     formTextarea: document.querySelectorAll('textarea'),
   };
   //================listener blur and write to local storage
-  [...formFitting.formInputs, ...formFitting.formTextarea].forEach(elemForm => {
-    elemForm.addEventListener('blur', e => {
-      // console.log(e.target.name)
-      if (e.target.name === 'name') {
-        localStorage.setItem('formFittingData-name', e.target.value);
-      } else if (e.target.name === 'tel') {
-        localStorage.setItem('formFittingData-tel', e.target.value);
-      } else if (e.target.name === 'date') {
-        localStorage.setItem('formFittingData-date', e.target.value);
-      } else if (e.target.name === 'comment') {
-        localStorage.setItem('formFittingData-comment', e.target.value);
-      }
-    });
-  });
+  [...formReviewsAskQuestion.formInputs, ...formReviewsAskQuestion.formTextarea].forEach(
+    elemForm => {
+      elemForm.addEventListener('blur', e => {
+        // console.log(e.target.name)
+        if (e.target.name === 'name') {
+          localStorage.setItem('formReviewsAskQuestionData-name', e.target.value);
+        } else if (e.target.name === 'tel') {
+          localStorage.setItem('formReviewsAskQuestionData-tel', e.target.value);
+        } else if (e.target.name === 'email') {
+          localStorage.setItem('formReviewsAskQuestionData-email', e.target.value);
+        } else if (e.target.name === 'comment') {
+          localStorage.setItem('formReviewsAskQuestionData-comment', e.target.value);
+        }
+      });
+    },
+  );
   //========================submit form to local storage + sync
-  formFitting.form.forEach(elem => {
+  formReviewsAskQuestion.form.forEach(elem => {
     elem.addEventListener('submit', e => {
       e.preventDefault();
-      const formFittingData = {
-        formFittingName: e.target.name.value,
-        formFittingTel: e.target.tel.value,
-        formFittingdate: e.target.date.value,
-        formFittingComment: e.target.comment.value,
+      const formReviewsAskQuestionData = {
+        formReviewsAskQuestionName: e.target.name.value,
+        formReviewsAskQuestionTel: e.target.tel.value,
+        formReviewsAskQuestionemail: e.target.email.value,
+        formReviewsAskQuestionComment: e.target.comment.value,
       };
-      localStorage.setItem('formFittingData', JSON.stringify(formFittingData)); //отправляет в localData
-      localStorage.removeItem('formFittingData-name');
-      localStorage.removeItem('formFittingData-tel');
-      localStorage.removeItem('formFittingData-date');
-      localStorage.removeItem('formFittingData-comment');
+      localStorage.setItem(
+        'formReviewsAskQuestionData',
+        JSON.stringify(formReviewsAskQuestionData),
+      ); //отправляет в localData
+      localStorage.removeItem('formReviewsAskQuestionData-name');
+      localStorage.removeItem('formReviewsAskQuestionData-tel');
+      localStorage.removeItem('formReviewsAskQuestionData-email');
+      localStorage.removeItem('formReviewsAskQuestionData-comment');
       e.target.name.value = '';
       e.target.tel.value = '';
-      e.target.date.value = '';
+      e.target.email.value = '';
       e.target.comment.value = '';
     });
     const getFormData = {
       name: elem.elements.name,
       tel: elem.elements.tel,
-      date: elem.elements.date,
+      email: elem.elements.email,
       comment: elem.elements.comment,
     };
-    getFormData.name.value = localStorage.getItem('formFittingData-name');
-    getFormData.tel.value = localStorage.getItem('formFittingData-tel');
-    getFormData.date.value = localStorage.getItem('formFittingData-date');
-    getFormData.comment.value = localStorage.getItem('formFittingData-comment');
-  });
-  //============see to block size
-  formFitting.box.forEach(elem => {
-    if (elem.offsetWidth > 499 && elem.offsetWidth < 800) {
-      elem.classList.add('in-page');
-      elem.querySelector('.form-fitting-showroom__title').classList.add('in-page');
-      elem.querySelector('.form-fitting-showroom__button').classList.add('in-page');
-      elem.querySelector('.form-fitting-showroom__comment').classList.add('in-page');
-      elem.querySelector('.form-fitting-showroom__comment-box').classList.add('in-page');
-      elem.querySelector('.form-fitting-showroom__input-center').classList.add('in-page');
-      elem.querySelector('.form-fitting-showroom__icon').classList.add('in-page');
-      const allHorizBlockFormFitting = elem.querySelectorAll('.form-fitting-showroom__horiz-block');
-      allHorizBlockFormFitting[0].classList.add('in-page');
-      allHorizBlockFormFitting[1].classList.add('in-page');
-      const allInputFormFitting = elem.querySelectorAll('.form-fitting-showroom__input');
-      allInputFormFitting[0].classList.add('in-page');
-      allInputFormFitting[1].classList.add('in-page');
-      allInputFormFitting[2].classList.add('in-page');
-      // console.log('в странице');
-    } else {
-      elem.classList.add('in-form');
-      elem.querySelector('.form-fitting-showroom__title').classList.add('in-form');
-      elem.querySelector('.form-fitting-showroom__button').classList.add('in-form');
-      elem.querySelector('.form-fitting-showroom__comment').classList.add('in-form');
-      elem.querySelector('.form-fitting-showroom__comment-box').classList.add('in-form');
-      elem.querySelector('.form-fitting-showroom__input-center').classList.add('in-form');
-      elem.querySelector('.form-fitting-showroom__icon').classList.add('in-form');
-      const allHorizBlockFormFitting = elem.querySelectorAll('.form-fitting-showroom__horiz-block');
-      allHorizBlockFormFitting[0].classList.add('in-form');
-      allHorizBlockFormFitting[1].classList.add('in-form');
-      const allInputFormFitting = elem.querySelectorAll('.form-fitting-showroom__input');
-      allInputFormFitting[0].classList.add('in-form');
-      allInputFormFitting[1].classList.add('in-form');
-      allInputFormFitting[2].classList.add('in-form');
-      // console.log('в форме');
-    }
+    getFormData.name.value = localStorage.getItem('formReviewsAskQuestionData-name');
+    getFormData.tel.value = localStorage.getItem('formReviewsAskQuestionData-tel');
+    getFormData.email.value = localStorage.getItem('formReviewsAskQuestionData-email');
+    getFormData.comment.value = localStorage.getItem('formReviewsAskQuestionData-comment');
   });
 }
--- src/js/layout/reviews/videoSet.js
+++ src/js/layout/reviews/videoSet.js
@@ -45,4 +45,43 @@ export function videosetSlickSettings() {
       ],
     });
   });
+  const reviewsRefs = {
+    play: document.querySelectorAll('.video-set-svg'),
+    video: document.querySelectorAll('.video-set__image'),
+    fullscreen: document.querySelectorAll('.video-set-fullscreen'),
+    link: document.querySelector('.videoset'),
+  };
+
+  reviewsRefs.play.forEach((element, index) => {
+    element.addEventListener('click', el => {
+      reviewsRefs.video[index].play();
+    });
+    reviewsRefs.video[index].addEventListener('click', ez => {
+      reviewsRefs.video[index].pause();
+    });
+    reviewsRefs.fullscreen[index].addEventListener('click', ez => {
+      if (reviewsRefs.video[index].requestFullscreen) {
+        reviewsRefs.video[index].requestFullscreen();
+      } else if (reviewsRefs.video[index].mozRequestFullScreen) {
+        /* Firefox */
+        reviewsRefs.video[index].mozRequestFullScreen();
+      } else if (reviewsRefs.video[index].webkitRequestFullscreen) {
+        /* Chrome, Safari and Opera */
+        reviewsRefs.video[index].webkitRequestFullscreen();
+      } else if (reviewsRefs.video[index].msRequestFullscreen) {
+        /* IE/Edge */
+        reviewsRefs.video[index].msRequestFullscreen();
+      }
+    });
+  });
+  reviewsRefs.video.forEach((videos, index) => {
+    videos.addEventListener('play', ez => {
+      reviewsRefs.play[index].style.display = 'none';
+      reviewsRefs.fullscreen[index].style.display = 'block';
+    });
+    videos.addEventListener('pause', ez => {
+      reviewsRefs.play[index].style.display = 'block';
+      reviewsRefs.fullscreen[index].style.display = 'none';
+    });
+  });
 }
