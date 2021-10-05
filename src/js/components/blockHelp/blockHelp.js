c14289e85fe30eab41a820ef52eedab6d764c769
NikitaMelnychencko <hunter6059@gmail.com>
2021-10-05 03:18:20 +0300

    +

--- src/js/components/blockHelp/blockHelp.js
+++ src/js/components/blockHelp/blockHelp.js
@@ -229,31 +229,31 @@ export function blockHelpRenderOpen() {
     // Вызываем функцию, первый аргумент - отступ, второй - скорость скролла, чем больше значение, тем медленнее скорость прокрутки
     scrollTo(0, 700);
   }
-  export function scrollTo(to, duration = 700) {
-    const element = document.scrollingElement || document.documentElement,
-      start = element.scrollTop,
-      change = to - start,
-      startDate = +new Date(),
-      // t = current time
-      // b = start value
-      // c = change in value
-      // d = duration
-      easeInOutQuad = function (t, b, c, d) {
-        t /= d / 2;
-        if (t < 1) return (c / 2) * t * t + b;
-        t--;
-        return (-c / 2) * (t * (t - 2) - 1) + b;
-      },
-      animateScroll = function () {
-        const currentDate = +new Date();
-        const currentTime = currentDate - startDate;
-        element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
-        if (currentTime < duration) {
-          requestAnimationFrame(animateScroll);
-        } else {
-          element.scrollTop = to;
-        }
-      };
-    animateScroll();
-  }
+}
+export function scrollTo(to, duration = 700) {
+  const element = document.scrollingElement || document.documentElement,
+    start = element.scrollTop,
+    change = to - start,
+    startDate = +new Date(),
+    // t = current time
+    // b = start value
+    // c = change in value
+    // d = duration
+    easeInOutQuad = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    },
+    animateScroll = function () {
+      const currentDate = +new Date();
+      const currentTime = currentDate - startDate;
+      element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+      if (currentTime < duration) {
+        requestAnimationFrame(animateScroll);
+      } else {
+        element.scrollTop = to;
+      }
+    };
+  animateScroll();
 }
