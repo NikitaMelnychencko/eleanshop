aefe9a292b0d54f0b4c4e8adfdfd1f776086819c
NikitaMelnychencko <hunter6059@gmail.com>
2021-10-05 21:27:38 +0300

    +

--- src/js/layout/fitting/informationAboutFitting.js
+++ src/js/layout/fitting/informationAboutFitting.js
@@ -1,5 +1,3 @@
 import informationAboutFitting from '../../../views/partials/fitting/informationAboutFitting.hbs';
-import informationAboutFitting_markupAboutSizeTable from '../../../views/layouts/fitting.hbs';
-const menuEl = document.querySelector('main')
 const informationAboutFitting_informationCreate = informationAboutFitting();
 export default informationAboutFitting_informationCreate
--- src/js/layout/fitting/sizeTable.js
+++ src/js/layout/fitting/sizeTable.js
@@ -2,8 +2,6 @@ import sizeTable_sizeTable from '../../../views/partials/fitting/sizeTable.hbs';
 import sizeTable_markupAboutSizeTable from '../../../views/layouts/fitting.hbs';
 import videoSlider_videoSliderCreate from './videoSlider.js'
 import informationAboutFitting_informationCreate from './informationAboutFitting.js'
-import '../../../images/img/elean47390.jpg'
-const menuEl = document.querySelector('main')
 const sizeTable_tableCreate = sizeTable_sizeTable();
-const fitting = sizeTable_markupAboutSizeTable( {sizeTable_tableCreate,videoSlider_videoSliderCreate,informationAboutFitting_informationCreate})
-menuEl.insertAdjacentHTML('beforeend',  fitting)
+export const fitting = sizeTable_markupAboutSizeTable( {sizeTable_tableCreate,videoSlider_videoSliderCreate,informationAboutFitting_informationCreate})
+
