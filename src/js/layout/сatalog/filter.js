62740fb3afe9c0097fc8cb082b3c7028b6bd8c41
LarysaTymoshenko <luda76@ukr.net>
2021-10-06 00:29:30 +0300

    029

--- src/js/layout/сatalog/filter.js
+++ src/js/layout/сatalog/filter.js
@@ -1,2 +1,13 @@
 import filterList from '../../json/filter.json';
 import filterListHbs from '../../../views/partials/сatalog/filter.hbs';
+
+
+
+export const setFilterHbs = filterListHbs(filterList);
+// 
+const filtersRefs = {
+   category : document.querySelectorAll('.filters-categor__item'),
+   collection : document.querySelectorAll('.filters-collection__item'),
+}
+
+
