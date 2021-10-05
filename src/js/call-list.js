c437a31bb700afed612d9d146f2e371a80961cee
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-10-06 02:54:18 +0300

    Integrate other sections to product info section

--- src/js/call-list.js
+++ src/js/call-list.js
@@ -108,7 +108,7 @@ function fittingRender() {
 }
 fittingRender()
 //=====product========//
-import productFunctions from './layout/product/infoAboutProduct.js'
+import { callProductPageFunctional, createFullMarkup } from './layout/product/infoAboutProduct.js'
 import ProductModalAddToCart from './layout/product/productModalAddToCart.js';
 import RecomendationsCategory from './layout/product/recomendationsCategory.js';
 import cards from './json/catalog.json';
@@ -117,8 +117,6 @@ import HandSewn from './layout/product/productHandSewn.js';
 import backdropMarkupTempl from '../views/components/backdrop.hbs';
 import { preorderMark, setEventPreorder } from './layout/product/preorderModal.js';
 import { tryOnModels, setEventTryOnModels } from './layout/product/tryOnModelsModal.js';
-
-const { createAllListeners, createFullMarkup } = productFunctions;
 
 function productRender() {
   const objRecomendationsCategory = new RecomendationsCategory({
@@ -145,7 +143,7 @@ function productRender() {
   const modalFormMarkup = objProductModalAddToCart.getMarkup();
   const backdropMarkup = backdropMarkupTempl(modalFormMarkup);
   const obj = {
-    infoAboutProduct: createFullMarkup(),
+    infoAboutProduct: createFullMarkup,
     recomendationCategory: objRecomendationsCategory.getMarkup(),
     handSewn: objHandSewn.getMarkup(),
     backdrop: backdropMarkup,
@@ -155,7 +153,7 @@ function productRender() {
   refs.mainEL.insertAdjacentHTML('beforeend', productMarkup(obj));
   // refs.mainEL.innerHTML = productMarkup(obj);
 
-  createAllListeners();
+  callProductPageFunctional(objProductModalAddToCart.show);
   document.querySelector('.form__button-сlose').style.display = 'none';
   objRecomendationsCategory.setSlider();
   objRecomendationsCategory.setEvent();
