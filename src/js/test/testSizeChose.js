cc9fed23eef793e63454833aeede145ad7581e8e
Dron010 <73987873+Dron010@users.noreply.github.com>
2021-10-06 23:56:06 +0300

    fix

--- src/js/test/testSizeChose.js
+++ src/js/test/testSizeChose.js
@@ -1,37 +1,37 @@
-// import sizeChose from '../components/sizeChose.js';
-// import size from '../json/sizeChose.json';
+import sizeChose from '../components/sizeChose.js';
+import size from '../json/sizeChose.json';
 
-// const {createMarkup, sizeListener, createBtn} = sizeChose;
+const {createMarkup, sizeListener, createBtn} = sizeChose;
 
-//  const body = document.querySelector('body');
-// body.insertAdjacentHTML('beforebegin', '<button data-modal-open>!!!!!!!BUTTON ON MODAL Size Chose !!!!!!</button>');
+ const body = document.querySelector('body');
+//body.insertAdjacentHTML('beforebegin', '<button data-modal-open>!!!!!!!BUTTON ON MODAL Size Chose !!!!!!</button>');
  
-//  const btnRef = document.querySelector('[data-modal-open]');
-// btnRef.addEventListener('click', onBtnClick);
+ const btnRef = document.querySelector('[data-modal-open]');
+//btnRef.addEventListener('click', onBtnClick);
 
-// import getRefs from '../refs/refs.js';
-// import Backdrop from '../components/backdrop.js';
-// import backdropMarkupTempl from '../../views/components/backdrop.hbs';
-// let throttle = require('lodash.throttle');
+import getRefs from '../refs/refs.js';
+import Backdrop from '../components/backdrop.js';
+import backdropMarkupTempl from '../../views/components/backdrop.hbs';
+let throttle = require('lodash.throttle');
 
-// function onBtnClick(event) {
-//   const backdropRef = document.querySelector('[data-modal]');
-//   const { mainEL } = getRefs;
-//   if (backdropRef === null) {
-//     const modalFormMarkup = createMarkup(createBtn(size));
-//     const backdropMarkup = backdropMarkupTempl(modalFormMarkup);
+function onBtnClick(event) {
+  const backdropRef = document.querySelector('[data-modal]');
+  const { mainEL } = getRefs;
+  if (backdropRef === null) {
+    const modalFormMarkup = createMarkup(createBtn(size));
+    const backdropMarkup = backdropMarkupTempl(modalFormMarkup);
 
-//     mainEL.insertAdjacentHTML('beforeend', backdropMarkup);
-//     sizeListener();
+    mainEL.insertAdjacentHTML('beforeend', backdropMarkup);
+    sizeListener();
 
-//     window.addEventListener('resize', throttle(onResize, 50));
-//   }
-//   const backdrop = new Backdrop();
-//   onResize();
-// }
-// function onResize(event) {
-//   let backdropRef = document.querySelector('[data-modal]');
-//   const right = (backdropRef.clientWidth - backdropRef.children[0].children[1].clientWidth) / 2;
-//   const btnCloseRef = document.querySelector('.form__button-сlose');
-//   btnCloseRef.style.right = `${right}px`;
-// }
+    window.addEventListener('resize', throttle(onResize, 50));
+  }
+  const backdrop = new Backdrop();
+  onResize();
+}
+function onResize(event) {
+  let backdropRef = document.querySelector('[data-modal]');
+  const right = (backdropRef.clientWidth - backdropRef.children[0].children[1].clientWidth) / 2;
+  const btnCloseRef = document.querySelector('.form__button-сlose');
+  btnCloseRef.style.right = `${right}px`;
+}
