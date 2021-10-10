8c81792b1d9d3f3902a2b18bc05b2372e2ee2e47
NikitaMelnychencko <hunter6059@gmail.com>
2021-10-11 00:36:12 +0300

    +

--- src/js/layout/static/footer.js
+++ src/js/layout/static/footer.js
@@ -1,220 +1,232 @@
 import refs from '../../refs/refs.js';
 import { scrollTo } from '../../components/blockHelp/blockHelp';
-import { catalogRender, fittingRender, brandRender, productRender, contactRender, reviewsRender, deliveryRender, favoritesRender, showroomRender, checkoutRender } from '../../call-list';
+import {
+  catalogRender,
+  fittingRender,
+  brandRender,
+  productRender,
+  contactRender,
+  reviewsRender,
+  deliveryRender,
+  favoritesRender,
+  showroomRender,
+  checkoutRender,
+} from '../../call-list';
 import { id } from 'postcss-selector-parser';
 import { indexOf } from 'lodash';
 
 const {
-    closeOpenPlus,
-    inputStorageMobile,
-    inputStorageDesktop,
-    checkBoxIcon,
-    agreeActive,
-    mobileSubmitBtn,
-    desktopSubmitBtn,
-    desktop,
-    linkMenuFooterDesktop,
-    linkMenuFooterMobile,
-    dropDownList,
-    dropDown,
-} = refs
+  closeOpenPlus,
+  inputStorageMobile,
+  inputStorageDesktop,
+  checkBoxIcon,
+  agreeActive,
+  mobileSubmitBtn,
+  desktopSubmitBtn,
+  desktop,
+  linkMenuFooterDesktop,
+  linkMenuFooterMobile,
+  dropDownList,
+  dropDown,
+} = refs;
 
-checkBoxIcon.addEventListener('click', onAgreeCheckBox)
-mobileSubmitBtn.addEventListener('submit', onSubmitBtnMobile)
-desktopSubmitBtn.addEventListener('submit', onSubmitBtnDesktop)
+checkBoxIcon.addEventListener('click', onAgreeCheckBox);
+mobileSubmitBtn.addEventListener('submit', onSubmitBtnMobile);
+desktopSubmitBtn.addEventListener('submit', onSubmitBtnDesktop);
 
 //=== smooth scrolling on  desktop
-desktop.forEach((evt) => {
-    evt.addEventListener('click', (el) => {
-        scrollTo(0, 700);
-        if (el.target) {
-        }
-    });
+desktop.forEach(evt => {
+  evt.addEventListener('click', el => {
+    scrollTo(0, 700);
+    if (el.target) {
+    }
+  });
 });
 
 //=== addEventListener Footer__Desktop on dataAction and LockalStorage===//
-linkMenuFooterDesktop.forEach((evt) => {
-    evt.addEventListener('click', (el) => {
-        const selected = el.target.dataset.atribute;
-        console.log(selected)
-        if (selected) {
-            localStorage.setItem("footer-filter-desktop", selected)
-        }
-    });
+linkMenuFooterDesktop.forEach(evt => {
+  evt.addEventListener('click', el => {
+    const selected = el.target.dataset.atribute;
+    if (selected) {
+      localStorage.setItem('footer-filter-desktop', selected);
+    }
+  });
 });
 
 //=== addEventListener Footer__Mobile on id and LockalStorage===//
-linkMenuFooterMobile.forEach((evt) => {
-    evt.addEventListener('click', (el) => {
-        const selected = el.target.dataset.atribute;
-        if (selected) {
-            localStorage.setItem("footer-filter-mobile", selected)
-        }
-    });
+linkMenuFooterMobile.forEach(evt => {
+  evt.addEventListener('click', el => {
+    const selected = el.target.dataset.atribute;
+    if (selected) {
+      localStorage.setItem('footer-filter-mobile', selected);
+    }
+  });
 });
 
 // === drop-down menu-list ===
-closeOpenPlus.forEach((evt) => {
-    evt.addEventListener('click', (el) => {
-        const idMuneClickMobile = evt.id;
-        if (!el.target.nextElementSibling) {
-            scrollTo(0, 700);
-            localStorage.setItem("footer-filtr-mobile", idMuneClickMobile)
+closeOpenPlus.forEach(evt => {
+  evt.addEventListener('click', el => {
+    const idMuneClickMobile = evt.id;
+    if (!el.target.nextElementSibling) {
+      scrollTo(0, 700);
+      localStorage.setItem('footer-filtr-mobile', idMuneClickMobile);
+    }
+    el.preventDefault();
+    if (el.target.nextElementSibling) {
+      if (dropDown) {
+        dropDown.classList.toggle('open-menu');
+        dropDown.nextElementSibling.classList.toggle('js-dropdown-none');
+        if (el.target === dropDown) {
+          return;
+        } else if (el.target === dropDownList) {
+          dropDown.parentElement.classList.remove('.js-dropdown-none');
         }
-        el.preventDefault()
-        if (el.target.nextElementSibling) {
-            if (dropDown) {
-                dropDown.classList.toggle('open-menu')
-                dropDown.nextElementSibling.classList.toggle('js-dropdown-none');
-                if (el.target === dropDown) {
-                    return;
-                } else if (el.target === dropDownList) {
-                    dropDown.parentElement.classList.remove('.js-dropdown-none');
-                }
-            }
-            el.target.classList.toggle('open-menu');
-            el.target.nextElementSibling.classList.toggle('js-dropdown-none');
-        } else {
-            window.location.href = el.target
-        }
-    });
+      }
+      el.target.classList.toggle('open-menu');
+      el.target.nextElementSibling.classList.toggle('js-dropdown-none');
+    } else {
+      window.location.href = el.target;
+    }
+  });
 });
 
 //==== Hiding the menu when switching to another block
-linkMenuFooterMobile.forEach((evt) => {
-    evt.addEventListener('click', (el) => {
-        scrollTo(0, 700);
-        el.preventDefault()
-        const dropDown = document.querySelector('.js-dropdown-none');
-        const openMenu = document.querySelector('.open-menu')
-        if (el.target) {
-            dropDown.classList.remove('js-dropdown-none')
-            openMenu.classList.remove('open-menu')
-            window.location.href = el.target
-        }
-    });
-
+linkMenuFooterMobile.forEach(evt => {
+  evt.addEventListener('click', el => {
+    scrollTo(0, 700);
+    el.preventDefault();
+    const dropDown = document.querySelector('.js-dropdown-none');
+    const openMenu = document.querySelector('.open-menu');
+    if (el.target) {
+      dropDown.classList.remove('js-dropdown-none');
+      openMenu.classList.remove('open-menu');
+      window.location.href = el.target;
+    }
+  });
 });
 
 // Activation deactivation checkbox
 function onAgreeCheckBox(evt) {
-    const iconCheck = evt.currentTarget
-    if (iconCheck) {
-        agreeActive.classList.toggle('js-show-and-remove')
-    }
+  const iconCheck = evt.currentTarget;
+  if (iconCheck) {
+    agreeActive.classList.toggle('js-show-and-remove');
+  }
 }
 
 // Appointment localStorage on input mobile
-inputStorageMobile.forEach((evt) => {
-    evt.addEventListener('input', (el) => {
-        const subscribe = el.currentTarget.value;
-        if (subscribe) {
-            localStorage.setItem(idInputMobile, subscribe)
-        }
-    });
+inputStorageMobile.forEach(evt => {
+  evt.addEventListener('input', el => {
+    const subscribe = el.currentTarget.value;
+    if (subscribe) {
+      localStorage.setItem(idInputMobile, subscribe);
+    }
+  });
 });
 
-
 // Appointment localStorage на input desktop
-inputStorageDesktop.forEach((evt) => {
-    const idInputDesktop = evt.id
-    evt.addEventListener('input', (el) => {
-        const subscribe = el.currentTarget.value;
-        if (subscribe) {
-            localStorage.setItem(idInputDesktop, subscribe)
-        }
-    });
+inputStorageDesktop.forEach(evt => {
+  const idInputDesktop = evt.id;
+  evt.addEventListener('input', el => {
+    const subscribe = el.currentTarget.value;
+    if (subscribe) {
+      localStorage.setItem(idInputDesktop, subscribe);
+    }
+  });
 });
 
 //   Remuve localStorage on input mobile
 function onSubmitBtnMobile(evt) {
-    evt.preventDefault();
-    evt.currentTarget.reset()
-    localStorage.removeItem('user_subscribe')
-
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('user_subscribe');
 }
 
 //   Remuve localStorage on input desktop
 function onSubmitBtnDesktop(evt) {
-    evt.preventDefault();
-    evt.currentTarget.reset()
-    localStorage.removeItem('name');
-    localStorage.removeItem('email');
-
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  localStorage.removeItem('name');
+  localStorage.removeItem('email');
 }
 
 //=== RENDER ===//
 
-const dataActionCollectio = document.querySelectorAll('[data-atribute]')
+const dataActionCollectio = document.querySelectorAll('[data-atribute]');
 
-dataActionCollectio.forEach((evt) => {
-    evt.addEventListener('click', (el) => {
-        const targetLink = el.target.dataset.atribute
-        //==== MobileRender ===//
+dataActionCollectio.forEach(evt => {
+  evt.addEventListener('click', el => {
+    const targetLink = el.target.dataset.atribute;
+    //==== MobileRender ===//
 
-        //==== catalogRender ===//
-        if (targetLink === 'autumn-winter' || targetLink === 'evening-sets' || targetLink === 'edding-and-graduation' || targetLink === 'the-tuxedo' || targetLink === 'costumes' || targetLink === 'pants' || targetLink === 'blouses') {
-            return catalogRender()
-        }
+    //==== catalogRender ===//
+    if (
+      targetLink === 'autumn-winter' ||
+      targetLink === 'evening-sets' ||
+      targetLink === 'edding-and-graduation' ||
+      targetLink === 'the-tuxedo' ||
+      targetLink === 'costumes' ||
+      targetLink === 'pants' ||
+      targetLink === 'blouses'
+    ) {
+      return catalogRender();
+    }
 
-        //==== brandRender ===//
-        if (targetLink === 'about-the-brand' || targetLink === 'about-founders' || targetLink === 'blog') {
-            return brandRender();
-        }
-        //==== showroomRender ===//
-        if (targetLink === 'showroom') {
-            return showroomRender();
-        }
+    //==== brandRender ===//
+    if (
+      targetLink === 'about-the-brand' ||
+      targetLink === 'about-founders' ||
+      targetLink === 'blog'
+    ) {
+      return brandRender();
+    }
+    //==== showroomRender ===//
+    if (targetLink === 'showroom') {
+      return showroomRender();
+    }
 
-        //==== deliveryRender ===//
-        if (targetLink === 'delivery' || targetLink === 'return' || targetLink === 'payment') {
-            return deliveryRender();
-        }
+    //==== deliveryRender ===//
+    if (targetLink === 'delivery' || targetLink === 'return' || targetLink === 'payment') {
+      return deliveryRender();
+    }
 
-        //==== showroomRender ===//
-        if (targetLink === 'showroom' || targetLink === 'showroom') {
-            return showroomRender();
-        }
+    //==== showroomRender ===//
+    if (targetLink === 'showroom' || targetLink === 'showroom') {
+      return showroomRender();
+    }
 
-        //==== formFittingInShowroom ===//
-        if (targetLink === 'fitting') {
-            return fittingRender();
-        }
+    //==== formFittingInShowroom ===//
+    if (targetLink === 'fitting') {
+      return fittingRender();
+    }
 
-        //==== reviewsRender ===//
-        if (targetLink === 'reviews') {
-            return reviewsRender();
-        }
+    //==== reviewsRender ===//
+    if (targetLink === 'reviews') {
+      return reviewsRender();
+    }
 
-        //==== renderDesktop ===//
+    //==== renderDesktop ===//
 
-        //==== DeliveryRender ===//
-        if (targetLink === 'Delivery' || targetLink === 'Payment' || targetLink === 'Return') {
-            return deliveryRender();
-        }
+    //==== DeliveryRender ===//
+    if (targetLink === 'Delivery' || targetLink === 'Payment' || targetLink === 'Return') {
+      return deliveryRender();
+    }
 
-        //==== FittingRender ===//
-        if (targetLink === 'Fitting') {
-            return fittingRender();
-        }
-        //==== ContactsRender ===//
-        if (targetLink === 'Contacts') {
-            return contactRender();
-        }
-    })
+    //==== FittingRender ===//
+      if (targetLink === 'Fitting') {
+      return fittingRender();
+    }
+    //==== ContactsRender ===//
+      if (targetLink === 'Contacts') {
+      return contactRender();
+    }
+  });
 });
 
 export function classBody(value) {
-    const BodyClass = "footer-switch";
-    if (BodyClass === value) {
-        document.body.classList.add(value)
-    } else {
-        document.body.classList.remove(BodyClass)
-    }
+  const BodyClass = 'footer-switch';
+  if (BodyClass === value) {
+    document.body.classList.add(value);
+  } else {
+    document.body.classList.remove(BodyClass);
+  }
 }
-
-
-
-
-
-
