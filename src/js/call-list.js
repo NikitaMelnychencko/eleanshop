f452e24fd26296b59550c8dac2163b75b56bcb1b
NikitaMelnychencko <hunter6059@gmail.com>
2021-12-19 19:03:50 +0200

    +

--- src/js/call-list.js
+++ src/js/call-list.js
@@ -275,7 +275,6 @@ export function reviewsRender() {
   videosetSlickSettings();
   starClientsSlider();
   starClientsComments();
-  formReviews();
   videoSetPlayer.clickListener();
   blockHelpRender();
   formReviews.init();
