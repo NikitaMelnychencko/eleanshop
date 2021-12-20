27f0990319be194aee16f68b42943fea90816039
NikitaMelnychencko <hunter6059@gmail.com>
2021-12-20 16:37:16 +0200

    add method

--- src/js/data/fetch_section.js
+++ src/js/data/fetch_section.js
@@ -1,15 +1,18 @@
 import { getSection } from './firebase_Servise.js';
 
+//======home======//
 function sectionHome() {
   const state = {};
   const home = getSection('home');
-  const components = getSection('components');
+  const components = getSection('components'); // разбить на разные файлы
   Promise.all([home, components]).then(values => {
     state.value = values;
   });
   return state;
 }
-function sectionProduct() {
+
+//======catalog=========//
+function sectionСatalog() {
   const state = {};
   const category = getSection('category');
   const products = getSection('products');
@@ -18,6 +21,9 @@ function sectionProduct() {
   });
   return state;
 }
+//=====product========//
+//????
+//=====brand========//
 function sectionBrand() {
   const state = {};
   getSection('aboutBrand').then(values => {
@@ -25,6 +31,7 @@ function sectionBrand() {
   });
   return state;
 }
+//=====checkout========//
 function sectionCheckout() {
   const state = {};
   getSection('ordering').then(values => {
@@ -32,3 +39,33 @@ function sectionCheckout() {
   });
   return state;
 }
+//=====contact========//
+function sectionContact() {
+  const state = {};
+  getSection('contact').then(values => {
+    state.value = values;
+  });
+  return state;
+}
+//=====delivery========//
+function sectionDelivery() {
+  const state = {};
+  getSection('delivery/questionsAnswers').then(values => {
+    state.value = values;
+  });
+  return state;
+}
+//=====fitting========//
+function sectionFitting() {
+  const state = {};
+  getSection('components/videoFitting').then(values => {
+    state.value = values;
+  });
+  return state;
+}
+
+//=====reviews========//
+//переиспользованая относитеьно home
+
+//=====showroom========//
+//переиспользованая относитеьно home
