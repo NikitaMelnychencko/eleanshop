94566fa65e44fbf833ba96ccd5f07dbe74054b03
Viktor Kostiuchenko <kostiuchenkoviktor@gmail.com>
2021-12-20 03:35:55 +0300

    Fix recomendation category

--- src/js/layout/product/recomendationsCategory.js
+++ src/js/layout/product/recomendationsCategory.js
@@ -81,31 +81,40 @@ export default class RecomendationsCategory {
   };
 
   setSlider = () => {
-    if (window.innerWidth <= 1377) {
+    // console.dir(window)
+    // const mediaQuery = window.matchMedia('(max-width: 1377px)')
+    // if (mediaQuery.matches) {
       this._addSlider();
-    }
+    // }
+    // if (window.innerWidth <= 1377) {
+    // }
+
   };
 
   _addSlider = () => {
     $(document).ready(function () {
       $('.recomendation-category .slider').slick({
         arrows: false,
-        dots: this.buttonPagination,
-        infinite: true,
-        slidesToShow: 2,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
         slidesToScroll: 1,
         speed: 1000,
         easing: 'ease',
+        infinite: true,
         initialSlide: 0,
         autoplay: true,
         autoplaySpeed: 2000,
-        focusOnSelect: true,
         pauseOnFocus: true,
         pauseOnHover: true,
         pauseOnDotsHover: true,
         draggable: true,
         swipe: true,
-        centerMode: true,
+        touchThreshold: 4,
+        touchMove: true,
+        waitForAnimate: true,
+        mobileFirst: true,
+        variableWidth: true,
       });
     });
   };
